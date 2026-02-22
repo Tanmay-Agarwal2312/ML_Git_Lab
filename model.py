9e45398d1f3df9d330bdbcdc3dcd62f021af55b0
Tanmay-Agarwal2312 <t.agarwal2312@gmail.com>
2026-02-23 00:29:23 +0530

    Adding Scaler

--- model.py
+++ model.py
@@ -13,7 +13,6 @@ X = pd.get_dummies(X, drop_first=True)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-
 scaler = StandardScaler()  
 X_train = scaler.fit_transform(X_train)  
 X_test = scaler.transform(X_test)
